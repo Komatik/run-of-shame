ce0072f385ed1da5b3f584ddf3b6cfbe8c04b799
Rserke <senne.p1@live.nl>
2017-03-09 09:07:12 +0100

    client.js aangepast

--- js/client.js
+++ js/client.js
@@ -5,14 +5,18 @@ document.getElementById("call_serverResponse").onclick = getData
 function getData(){
     $.get("/data", function(finishers){
         $.each(finishers,function(index, finisher){
-            var table = $("#server_response")
+            var table = document.getElementById("server_response")
             var rij = table.insertRow()
+
             var celNaam = rij.insertCell()
             celNaam.innerHTML = finisher.naam
+
             var celVoornaam = rij.insertCell()
             celVoornaam.innerHTML = finisher.voornaam
+            
             var celGender = rij.insertCell()
             celGender.innerHTML = finisher.gender
+            
             var celTijd = rij.insertCell()
             celTijd.innerHTML = finisher.minuten + ":" + finisher.seconden
         })
